035cd8950d484f7dc7278d3caa82802edc65f726
socha23 <socha23@gmail.com>
2016-01-02 22:22:04 +0100

    Initial project
    Project initialized from dropwizard-tutorial

---
+++ src/main/java/pl/socha23/HelloWorldApplication.java
@@ -0,0 +1,41 @@
+package pl.socha23;
+
+import io.dropwizard.Application;
+import io.dropwizard.assets.AssetsBundle;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+import pl.socha23.health.TemplateHealthCheck;
+import pl.socha23.resources.HelloWorldResource;
+
+/**
+ *
+ */
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        bootstrap.addBundle(new AssetsBundle("/assets", "/", "index.html"));
+
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration, Environment environment) throws Exception {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+        environment.jersey().register(resource);
+
+        final TemplateHealthCheck healthCheck = new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+    }
+}
