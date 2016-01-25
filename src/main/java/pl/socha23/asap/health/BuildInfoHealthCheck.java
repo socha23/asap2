0f4889e11edf804d1ca33da58dd7f02e231d57ee
socha23 <socha23@gmail.com>
2016-01-25 22:52:11 +0100

    removed unnecessary variable

--- src/main/java/pl/socha23/asap/health/BuildInfoHealthCheck.java
+++ src/main/java/pl/socha23/asap/health/BuildInfoHealthCheck.java
@@ -24,7 +24,7 @@ public class BuildInfoHealthCheck extends HealthCheck {
     protected Result check() throws Exception {
         try {
             URL url = Resources.getResource(buildInfoFile);
-            String info = Resources.toString(url, Charsets.UTF_8);
+            Resources.toString(url, Charsets.UTF_8);
             return Result.healthy();
         } catch (Exception e) {
             return Result.unhealthy("Can't read build info from " + buildInfoFile);
